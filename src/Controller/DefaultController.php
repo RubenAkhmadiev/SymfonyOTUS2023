ada81a7a3e80280d3dfa755224575b99c8c499bf
Airat Akhmetshin <st1ncnpk@gmail.com>
2023-05-13 14:27:25 +0400

    Подключил framework-extra-bundle, роут по-умолчанию

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    #[Route(name: '/', methods: ['GET'])]
+    public function index(): Response
+    {
+        return new Response('Default page');
+    }
+}
