26672c13bf65b0850ef43fadf825b961a0d69180
Ruben.Ahmadiev <rubysail97@yandex.ru>
2023-06-26 09:11:29 +0300

    User, Order, Item, UserProfile entities added

---
+++ src/Entity/Order.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $number;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private \DateTimeInterface $creation_date;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    #[ORM\Column]
+    private float $sum;
+
+    #[ORM\ManyToMany(targetEntity: Item::class, mappedBy: 'order')]
+    private Collection $items;
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): static
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getCreationDate(): \DateTimeInterface
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creation_date): static
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+
+    public function getUserId(): User
+    {
+        return $this->user;
+    }
+
+    public function setUserId(User $userId): static
+    {
+        $this->user = $userId;
+
+        return $this;
+    }
+
+    public function getSum(): float
+    {
+        return $this->sum;
+    }
+
+    public function setSum(float $sum): static
+    {
+        $this->sum = $sum;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Item>
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(Item $item): static
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->addOrderId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeItem(Item $item): static
+    {
+        if ($this->items->removeElement($item)) {
+            $item->removeOrderId($this);
+        }
+
+        return $this;
+    }
+}
