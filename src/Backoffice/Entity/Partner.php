e5d95ad9f30f23597536f931595b8790f884a752
Airat Akhmetshin <st1ncnpk@gmail.com>
2023-07-04 23:38:08 +0300

    Partners, categories

--- src/Backoffice/Entity/Partner.php
+++ src/Backoffice/Entity/Partner.php
@@ -2,14 +2,80 @@
 
 namespace App\Backoffice\Entity;
 
+use App\Backoffice\Enum\PartnerTypeEnum;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: '`partner`', schema: 'backoffice')]
+#[ORM\HasLifecycleCallbacks]
 class Partner
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 25)]
+    private string $type;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $createdAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $updatedAt;
+
+    #[ORM\PrePersist]
+    public function onPrePersist(): void
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    #[ORM\PreUpdate]
+    public function onPreUpdate(): void
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): PartnerTypeEnum
+    {
+        return PartnerTypeEnum::create($this->type);
+    }
+
+    public function setType(PartnerTypeEnum $type): static
+    {
+        $this->type = $type->getName();
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
 }
