c3346a2ec563d3b8e057bec082db844bcbd35310
Ruben.Ahmadiev <rubysail97@yandex.ru>
2023-07-17 21:23:28 +0300

    migration fix

--- migrations/Version20230717061345.php
+++ migrations/Version20230717061345.php
@@ -16,8 +16,8 @@ final class Version20230717061345 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('DROP TABLE IF EXISTS item');
         $this->addSql('DROP TABLE IF EXISTS item_order');
+        $this->addSql('DROP TABLE IF EXISTS item');
         $this->addSql('CREATE TABLE product_order (product_id INT NOT NULL, order_id INT NOT NULL, PRIMARY KEY(product_id, order_id))');
         $this->addSql('CREATE INDEX IDX_DF8E8848126F525E ON product_order (product_id)');
         $this->addSql('CREATE INDEX IDX_DF8E88488D9F6D38 ON product_order (order_id)');
